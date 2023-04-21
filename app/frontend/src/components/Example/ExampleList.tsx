072fbb19fd408b74483a56fb84a2a3ef8359da45
cruise <ceikoor@gmail.com>
2023-04-21 20:45:01 +0800

    Update ExampleList.tsx

--- app/frontend/src/components/Example/ExampleList.tsx
+++ app/frontend/src/components/Example/ExampleList.tsx
@@ -9,11 +9,11 @@ export type ExampleModel = {
 
 const EXAMPLES: ExampleModel[] = [
     {
-        text: "What is included in my Northwind Health Plus plan that is not in standard?",
-        value: "What is included in my Northwind Health Plus plan that is not in standard?"
+        text: "What are the benefits of SASE?",
+        value: "What are the benefits of SASE?"
     },
-    { text: "What happens in a performance review?", value: "What happens in a performance review?" },
-    { text: "What does a Product Manager do?", value: "What does a Product Manager do?" }
+    { text: "What are the benefits of Teams Call?", value: "What are the benefits of Teams Call?" },
+    { text: "What are the benefits of SIEM?", value: "What are the benefits of SIEM?" }
 ];
 
 interface Props {
